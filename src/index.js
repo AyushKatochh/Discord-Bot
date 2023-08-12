b32b804d6ae257709d56fa8ea835491b82b02e19
AyushKatochhh <ayush.k@w3dev.email>
2023-08-12 13:15:07 +0530

    v1

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const {Client, IntentsBitField} = require('discord.js');
+require('dotenv').config();
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent
+    ]
+})
+
+client.on('ready', (c) => {
+    console.log(`${c.user.tag} is online`);
+})
+
+client.on('messageCreate', (message) => {
+  if(message.author.bot) {
+     return;
+  }
+
+  if(message.content === "Hello") {
+    message.reply("Hello")
+  }
+}) 
+
+client.login(process.env.TOKEN)
